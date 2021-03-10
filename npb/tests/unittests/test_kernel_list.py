28bd44734175ff1009a504851a887b22c342c715
Marc Costa Sitja <Marc.Costa.Sitja@jpl.nasa.gov>
2021-03-10 09:49:04 -0800

    Defined kernel list generation test for MSL.

--- npb/tests/unittests/test_kernel_list.py
+++ npb/tests/unittests/test_kernel_list.py
@@ -16,12 +16,31 @@ class TestKernelList(TestCase):
     directory.
 
     """
+
     def test_basic(self):
+        """
+        Basic test for MSL kernel list generation. Implemented following
+        the generation of the MSK kernel list for release 26.
+
+        """
 
         config = 'data/msl_list.json'
         plan   = 'data/msl_release_26.plan'
 
+
         main(config,plan)
+
+        new_file = ''
+        with open('msl_release_26.kernel_list', 'r') as f:
+            for line in f:
+                new_file += line
+
+        old_file = ''
+        with open('data/msl_release_26.kernel_list', 'r') as f:
+            for line in f:
+                old_file += line
+
+        self.assertEqual(old_file.split('\n')[2:],new_file.split('\n')[2:])
 
         os.remove('msl_release_26.kernel_list')
 
