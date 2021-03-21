0bdc81841d9779f26164d46b396cd21170b147c9
Marc Costa Sitja <mcosta@sciops.esa.int>
2021-03-21 18:50:17 +0100

    Adapted tests.

--- npb/tests/unittests/test_kernel_list.py
+++ npb/tests/unittests/test_kernel_list.py
@@ -17,14 +17,15 @@ class TestKernelList(TestCase):
 
     """
 
-    def test_basic(self):
+    def test_pds3_list(self):
         """
-        Basic test for MSL kernel list generation. Implemented following
-        the generation of the MSK kernel list for release 26.
+        Basic test for MSL kernel list generation. This is a PDS3 data set.
+        Implemented following the generation of the kernel list for
+        release 26.
 
         """
 
-        config = 'data/msl_list.json'
+        config = 'data/msl_release_26.json'
         plan   = 'data/msl_release_26.plan'
 
 
@@ -45,4 +46,30 @@ class TestKernelList(TestCase):
         os.remove('msl_release_26.kernel_list')
 
 
+    def test_pds4_list(self):
+        """
+        Basic test for InSight kernel list generation. This is a PDS4 Bundle.
+        Implemented following the generation of the kernel list for release 8.
+
+        """
+
+        config = 'data/insight_release_08.json'
+        plan   = 'data/insight_release_08.plan'
+
+
+        main(config,plan)
+
+        new_file = ''
+        with open('insight_release_08.kernel_list', 'r') as f:
+            for line in f:
+                new_file += line
+
+        old_file = ''
+        with open('data/insight_release_08.kernel_list', 'r') as f:
+            for line in f:
+                old_file += line
+
+        self.assertEqual(old_file.split('\n')[9:],new_file.split('\n')[9:])
+
+        os.remove('insight_release_08.kernel_list')
 
