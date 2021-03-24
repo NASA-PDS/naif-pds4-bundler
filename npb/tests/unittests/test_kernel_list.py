8064e1d98dcd0c8807e2acd74f6520492045e5df
Marc Costa Sitja <Marc.Costa.Sitja@jpl.nasa.gov>
2021-03-24 10:23:56 -0700

    Updated unittests with M01 release 75.

--- npb/tests/unittests/test_kernel_list.py
+++ npb/tests/unittests/test_kernel_list.py
@@ -17,7 +17,7 @@ class TestKernelList(TestCase):
 
     """
 
-    def test_pds3_list(self):
+    def test_pds3_msl_list(self):
         """
         Basic test for MSL kernel list generation. This is a PDS3 data set.
         Implemented following the generation of the kernel list for
@@ -28,11 +28,10 @@ class TestKernelList(TestCase):
         config = 'data/msl_release_26.json'
         plan   = 'data/msl_release_26.plan'
 
-
-        main(config,plan)
+        main(config=config, plan=plan, silent=True)
 
         new_file = ''
-        with open('msl_release_26.kernel_list', 'r') as f:
+        with open('working/msl_release_26.kernel_list', 'r') as f:
             for line in f:
                 new_file += line
 
@@ -43,10 +42,40 @@ class TestKernelList(TestCase):
 
         self.assertEqual(old_file.split('\n')[9:],new_file.split('\n')[9:])
 
-        os.remove('msl_release_26.kernel_list')
+        os.remove('working/msl_release_26.kernel_list')
 
 
-    def test_pds4_list(self):
+    def test_pds3_m01_list(self):
+        """
+        Basic test for M01 kernel list generation. This is a PDS3 data set.
+        Implemented following the generation of the kernel list for
+        release 75.
+
+        """
+
+        config = 'data/m01_release_75.json'
+        plan   = 'data/m01_release_75.plan'
+
+
+        main(config=config, plan=plan, silent=True)
+
+        new_file = ''
+        with open('working/m01_release_75.kernel_list', 'r') as f:
+            for line in f:
+                new_file += line
+
+        old_file = ''
+        with open('data/m01_release_75.kernel_list', 'r') as f:
+            for line in f:
+                old_file += line
+
+        self.assertEqual(old_file.split('\n')[9:],new_file.split('\n')[9:])
+
+        os.remove('working/m01_release_75.kernel_list')
+
+
+
+    def test_pds4_insight_list(self):
         """
         Basic test for InSight kernel list generation. This is a PDS4 Bundle.
         Implemented following the generation of the kernel list for release 8.
@@ -56,11 +85,10 @@ class TestKernelList(TestCase):
         config = 'data/insight_release_08.json'
         plan   = 'data/insight_release_08.plan'
 
-
-        main(config,plan)
+        main(config=config, plan=plan, silent=True)
 
         new_file = ''
-        with open('insight_release_08.kernel_list', 'r') as f:
+        with open('working/insight_release_08.kernel_list', 'r') as f:
             for line in f:
                 new_file += line
 
@@ -71,5 +99,5 @@ class TestKernelList(TestCase):
 
         self.assertEqual(old_file.split('\n')[9:],new_file.split('\n')[9:])
 
-        os.remove('insight_release_08.kernel_list')
+        os.remove('working/insight_release_08.kernel_list')
 
