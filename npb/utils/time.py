f3dab9d78fd89f7c28ca1c3fe187b9653d4e1648
Marc Costa Sitja <Marc.Costa.Sitja@jpl.nasa.gov>
2021-04-10 13:27:16 -0700

    Moved coverage functions from product file.

--- npb/utils/time.py
+++ npb/utils/time.py
@@ -1,5 +1,6 @@
 import datetime
 import calendar
+import spiceypy
 import os
 
 
@@ -40,6 +41,139 @@ def creation_date(path):
     date = calendar.month_name[int(date[0:2])] + date[2:]
 
     return creation_date
+
+
+def spk_coverage(path):
+
+    ids = spiceypy.spkobj(path)
+
+    MAXIV = 1000
+    WINSIZ = 2 * MAXIV
+    TIMLEN = 62
+
+    coverage = spiceypy.support_types.SPICEDOUBLE_CELL(WINSIZ)
+
+    start_points_list = list()
+    end_points_list = list()
+
+    for id in ids:
+
+        spiceypy.scard, 0, coverage
+        spiceypy.spkcov(spk=path, idcode=id, cover=coverage)
+
+        num_inter = spiceypy.wncard(coverage)
+
+        for i in range(0, num_inter):
+            endpoints = spiceypy.wnfetd(coverage, i)
+
+            start_points_list.append(endpoints[0])
+            end_points_list.append(endpoints[1])
+
+
+    start_time = min(start_points_list)
+    stop_time = max(end_points_list)
+
+    start_time_cal = spiceypy.timout(start_time,
+                                     "YYYY-MM-DDTHR:MN:SC.###::UTC::RND", TIMLEN) + 'Z'
+    stop_time_cal = spiceypy.timout(stop_time,
+                                    "YYYY-MM-DDTHR:MN:SC.###::UTC::RND", TIMLEN) + 'Z'
+
+    return [start_time_cal, stop_time_cal]
+
+
+def ck_coverage(path):
+
+    start_points_list = list()
+    end_points_list = list()
+
+    MAXIV = 10000
+    WINSIZ = 2 * MAXIV
+    TIMLEN = 500
+    MAXOBJ = 10000
+
+    ids = spiceypy.support_types.SPICEINT_CELL(MAXOBJ)
+    ids = spiceypy.ckobj(ck=path, out_cell=ids)
+
+    for id in ids:
+
+        coverage = spiceypy.support_types.SPICEDOUBLE_CELL(WINSIZ)
+        spiceypy.scard, 0, coverage
+        coverage = spiceypy.ckcov(ck=path, idcode=id, needav=False,
+                                level='SEGMENT', tol=0.0, timsys='TDB',
+                                cover=coverage)
+
+        num_inter = spiceypy.wncard(coverage)
+
+        for j in range(0, num_inter):
+            endpoints = spiceypy.wnfetd(coverage, j)
+
+            start_points_list.append(endpoints[0])
+            end_points_list.append(endpoints[1])
+
+
+    start_time = min(start_points_list)
+    stop_time = max(end_points_list)
+
+    start_time_cal = spiceypy.timout(start_time,
+                                   "YYYY-MM-DDTHR:MN:SC.###::UTC::RND", TIMLEN) + 'Z'
+    stop_time_cal = spiceypy.timout(stop_time, "YYYY-MM-DDTHR:MN:SC.###::UTC::RND",
+                                  TIMLEN) + 'Z'
+
+    return [start_time_cal, stop_time_cal]
+
+#
+# PCK kernel processing
+#
+def pck_coverage(path):
+
+    MAXIV = 1000
+    WINSIZ = 2 * MAXIV
+    TIMLEN = 62
+    MAXOBJ = 1000
+
+    ids = spiceypy.support_types.SPICEINT_CELL(MAXOBJ)
+
+    spiceypy.pckfrm(path, ids)
+
+    coverage = spiceypy.support_types.SPICEDOUBLE_CELL(WINSIZ)
+
+    start_points_list = list()
+    end_points_list = list()
+
+    for id in ids:
+
+        spiceypy.scard, 0, coverage
+        spiceypy.pckcov(pck=path, idcode=id, cover=coverage)
+
+        num_inter = spiceypy.wncard(coverage)
+
+        for i in range(0, num_inter):
+            endpoints = spiceypy.wnfetd(coverage, i)
+
+            start_points_list.append(endpoints[0])
+            end_points_list.append(endpoints[1])
+
+    start_time_tbd = min(start_points_list)
+    stop_time_tbd = max(end_points_list)
+
+    start_time_cal = spiceypy.timout(start_time_tbd,
+                                   "YYYY-MM-DDTHR:MN:SC.###::UTC::RND", TIMLEN) + 'Z'
+    stop_time_cal = spiceypy.timout(stop_time_tbd,
+                                  "YYYY-MM-DDTHR:MN:SC.###::UTC::RND", TIMLEN) + 'Z'
+
+
+    return [start_time_cal, stop_time_cal]
+
+#
+# DSK kernel processing
+#
+def dsk_coverage(self):
+
+    start_time_cal = self.setup.mission_start
+    stop_time_cal  = self.setup.mission_stop
+
+    return [start_time_cal, stop_time_cal]
+
 
 
 def PDS3_label_gen_date(file):
