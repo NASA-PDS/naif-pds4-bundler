109ef254504bba8153b9fffe892e4697f5a8298d
Marc Costa Sitja <Marc.Costa.Sitja@jpl.nasa.gov>
2021-04-27 11:08:18 -0700

    Added verbosity option.

--- npb/classes/log.py
+++ npb/classes/log.py
@@ -7,25 +7,26 @@ import datetime
 
 class Log(object):
 
-    def __init__(self, setup, log_file=False, silent=False):
+    def __init__(self, setup, args):
 
         self.setup = setup
+        self.args = args
 
         logger = logging.getLogger()
         logger.setLevel(logging.INFO)
         log_format = '%(levelname)-8s %(module)-12s %(funcName)-20s %(message)s'
 
-        if not silent:
+        if args.verbose:
             ch = logging.StreamHandler()
             ch.setLevel(logging.INFO)
             formatter = logging.Formatter(log_format)
             ch.setFormatter(formatter)
             logger.addHandler(ch)
 
-        if log_file:
+        if args.log:
 
             log_file = setup.working_directory + os.sep + \
-                       f'{self.setup.mission_accronym}_release_temp.log'
+                       f'{setup.mission_accronym}_release_temp.log'
 
             if os.path.exists(log_file):
                 os.remove(log_file)
@@ -43,23 +44,43 @@ class Log(object):
 
     def start(self):
         start_message = f'naif-pd4-bundle-{self.setup.version} for ' \
-                        f'{self.setup.mission_accronym} run on '     \
-                        f'{socket.gethostname()} started at '        \
+                        f'{self.setup.readme["spice_name"]} run on '     \
+                        f'{socket.gethostname()} started  at '        \
                         f'{str(datetime.datetime.now())[:-7]}'
         logging.info(start_message)
         logging.info('='*len(start_message))
+        logging.info('')
+        if not self.setup.args.silent and not self.setup.args.verbose:
+            print(start_message + '\n'  + '=' * len(start_message))
+
+        #
+        # Display the arguments
+        #
+        logging.info('-- The following arguments have been provided:')
+        argument_dict = self.args.__dict__
+        whitespaces = len(max(argument_dict.keys(), key=len))
+
+        for attribute in argument_dict:
+            if argument_dict[attribute]:
+                logging.info(f'     {attribute}: {" "*(whitespaces-len(attribute))}{argument_dict[attribute]}')
+
         logging.info('')
 
         return
 
 
     def stop(self):
+        stop_message = f'naif-pd4-bundle-{self.setup.version} for {self.setup.readme["spice_name"]} run on ' \
+                       f'{socket.gethostname()} finished at '                                        \
+                       f'{str(datetime.datetime.now())[:-7]}'
         logging.info('')
-        logging.info(f'naif-pd4-bundle-{self.setup.version} for {self.setup.mission_name} run on '
-                     f'{socket.gethostname()} finished at '
-                     f'{str(datetime.datetime.now())[:-7]}')
+        logging.info('')
+        logging.info(stop_message)
         logging.info('')
         logging.info('End of log.')
+        if not self.setup.args.silent and not self.setup.args.verbose:
+            print('=' * len(stop_message))
+            print(stop_message)
 
         #
         # Rename the log file according to the version
@@ -82,5 +103,5 @@ def error_message(message):
 
     spiceypy.kclear()
 
-    raise Exception(error)
+    raise RuntimeError(error)
 
