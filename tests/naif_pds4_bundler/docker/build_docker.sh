e961b6bb2b1881670aa3f49203741109ea57a965
Marc Costa Sitja <Marc.Costa.Sitja@jpl.nasa.gov>
2022-03-29 15:51:33 -0700

    Updates to integrate CI CD with PDS-EN GitHub repo.

--- tests/naif_pds4_bundler/docker/build_docker.sh
+++ tests/naif_pds4_bundler/docker/build_docker.sh
@@ -1,5 +1,7 @@
 TAG_NAME='naif-pds4-bundler:latest'
-mkdir npb
-cd npb
 git clone https://github-fn.jpl.nasa.gov/NAIF/naif-pds4-bundler.git
+git checkout develop
 sudo docker build . -t $TAG_NAME
+
+# Access the VM with
+# docker run -it naif-pds4-bundler:latest
