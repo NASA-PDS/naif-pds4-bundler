24619472ede4f0e764579679875571c823c9a1e5
Jorge Diaz del Rio <jdiazdelrio@odcspace.com>
2026-02-08 01:21:06 +0100

    Update test family format: moved test_spk_coverage to the right place within the file and added missing blank lines.

--- tests/npb/test_utils_time.py
+++ tests/npb/test_utils_time.py
@@ -7,6 +7,7 @@ import spiceypy
 
 from pds.naif_pds4_bundler.utils import time
 
+
 # Get the directory where the data is located.
 KERNELS = Path(__file__).parent.parent / "naif_pds4_bundler" / "data" / "kernels"
 
@@ -26,6 +27,7 @@ def m2020_fk():
     yield kernel
     spiceypy.unload(kernel)
 
+
 def test_dsk_coverage(lsk):
     """Test DSK coverage function using pytest."""
     dsk_file = str( KERNELS/ "dsk" / "DEIMOS_K005_THO_V01.BDS")
@@ -35,17 +37,6 @@ def test_dsk_coverage(lsk):
     assert (start_time_cal, stop_time_cal) == (
         "1950-01-01T00:00:00.000Z",
         "2049-12-31T23:59:59.000Z"
-    )
-
-
-def test_spk_coverage(lsk, m2020_fk):
-    """Test SPK coverage function."""
-    spk_file = str( KERNELS/ "spk" / "m2020_surf_rover_loc_0000_0089_v1.bsp")
-    [start_time_cal, stop_time_cal] = time.spk_coverage(spk_file, main_name="M2020")
-
-    assert (start_time_cal, stop_time_cal) == (
-        "2021-02-18T21:52:40.482Z",
-        "2021-05-21T15:47:07.765Z"
     )
 
 
@@ -60,3 +51,14 @@ def test_parse_date(date_input, expected):
 
     assert isinstance(result, datetime)
     assert result == expected
+
+
+def test_spk_coverage(lsk, m2020_fk):
+    """Test SPK coverage function."""
+    spk_file = str( KERNELS/ "spk" / "m2020_surf_rover_loc_0000_0089_v1.bsp")
+    [start_time_cal, stop_time_cal] = time.spk_coverage(spk_file, main_name="M2020")
+
+    assert (start_time_cal, stop_time_cal) == (
+        "2021-02-18T21:52:40.482Z",
+        "2021-05-21T15:47:07.765Z"
+    )
