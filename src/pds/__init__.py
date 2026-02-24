3ca4b6083f2effdb899ccfb7abfa1f4dcc532a0f
Alyssa Bailey <alyssa.m.bailey@jpl.nasa.gov>
2026-02-24 08:58:51 -0800

    steps 1 and 2 on GH issue 53

--- src/pds/__init__.py
+++ src/pds/__init__.py
@@ -1,2 +1,3 @@
 """PDS Namespace."""
-__import__("pkg_resources").declare_namespace(__name__)
+#__import__("pkg_resources").declare_namespace(__name__)
+#removed for migrating pds namespace to PEP 420
