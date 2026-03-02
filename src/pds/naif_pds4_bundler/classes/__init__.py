6ac85f48a1927a2b92ea0b6cd350166410b2f724
Alyssa Bailey <alyssa.m.bailey@jpl.nasa.gov>
2026-03-02 15:27:40 -0800

    made updates to remove runtime dependency on pkg_resources

--- src/pds/naif_pds4_bundler/classes/__init__.py
+++ src/pds/naif_pds4_bundler/classes/__init__.py
@@ -2,3 +2,10 @@
 
 The NPB Classes implement the main functionalities of the pipeline.
 """
+from importlib.resources import files
+__version__ = VERSION = (
+    files(__package__)
+    .joinpath("VERSION.txt")
+    .read_text(encoding="utf-8")
+    .strip()
+)
--- tests/npb/test__init.py
+++ tests/npb/test__init.py
@@ -8,3 +8,12 @@ def test_pds_namespace_import():
 
     assert 'pds' in sys.modules
     assert 'pds.naif_pds4_bundler' in sys.modules
+
+
+def test_version_string_loaded():
+    import pds.naif_pds4_bundler
+
+    version = pds.naif_pds4_bundler.__version__
+
+    assert isinstance(version, str)
+    assert len(version) > 0
