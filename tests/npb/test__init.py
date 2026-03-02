a9d3e892b2476f21cfa88ce049b1e341fdf66117
Alyssa Bailey <alyssa.m.bailey@jpl.nasa.gov>
2026-03-02 15:50:07 -0800

    made updates to remove runtime dependency on pkg_resources

--- tests/npb/test__init.py
+++ tests/npb/test__init.py
@@ -3,17 +3,17 @@ import importlib
 
 
 def test_pds_namespace_import():
-    importlib.import_module('pds')
-    importlib.import_module('pds.naif_pds4_bundler')
+     importlib.import_module('pds')
+     importlib.import_module('pds.naif_pds4_bundler')
 
-    assert 'pds' in sys.modules
-    assert 'pds.naif_pds4_bundler' in sys.modules
+     assert 'pds' in sys.modules
+     assert 'pds.naif_pds4_bundler' in sys.modules
 
 
 def test_version_string_loaded():
-    import pds.naif_pds4_bundler
+     import pds.naif_pds4_bundler
 
-    version = pds.naif_pds4_bundler.__version__
+     version = pds.naif_pds4_bundler.__version__
 
-    assert isinstance(version, str)
-    assert len(version) > 0
+     assert isinstance(version, str)
+     assert len(version) > 0
