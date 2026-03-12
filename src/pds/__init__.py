a66fe4ae68141ead7867ef6ecc952a0819d19a98
Jorge Diaz del Rio <Jorge.Diaz.del.Rio@odcspace.com>
2026-03-12 15:57:41 +0100

    copied contents of __init__ file from pds directory to the __init__ file in naif_pds4_bundler and removed the file in pds directory.

--- src/pds/__init__.py
+++
@@ -1,9 +0,0 @@
-"""PDS Namespace."""
-
-from importlib.resources import files
-__version__ = VERSION = (
-    files(__package__)
-    .joinpath("VERSION.txt")
-    .read_text(encoding="utf-8")
-    .strip()
-)
--- src/pds/naif_pds4_bundler/__init__.py
+++ src/pds/naif_pds4_bundler/__init__.py
@@ -1,11 +1,9 @@
 """NAIF PDS4 Bundle Namespace."""
-import pkg_resources
+from importlib.resources import files
 
 __version__ = VERSION = (
-    pkg_resources.resource_string(__name__, "VERSION.txt").decode("utf-8").strip()
+    files(__package__)
+    .joinpath("VERSION.txt")
+    .read_text(encoding="utf-8")
+    .strip()
 )
-
-# For future consideration:
-#
-# - Other metadata (__docformat__, __copyright__, etc.)
-# - N̶a̶m̶e̶s̶p̶a̶c̶e̶ ̶p̶a̶c̶k̶a̶g̶e̶s̶ we got this
