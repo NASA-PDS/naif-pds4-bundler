9c12bbcda161487604ea133cd83667c8c6365ee8
Jorge Diaz del Rio <Jorge.Diaz.del.Rio@odcspace.com>
2026-03-12 17:59:29 +0100

    Added backport package for importlib.resources for Python3.8, to be removed when that version is discontinued.

--- src/pds/naif_pds4_bundler/__init__.py
+++ src/pds/naif_pds4_bundler/__init__.py
@@ -1,5 +1,8 @@
 """NAIF PDS4 Bundle Namespace."""
-from importlib.resources import files
+try:
+    from importlib.resources import files
+except ImportError:  # Python 3.8
+    from importlib_resources import files
 
 __version__ = VERSION = (
     files(__package__)
