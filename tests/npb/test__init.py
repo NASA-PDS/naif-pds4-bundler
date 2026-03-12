ffe1a01d2399eaed770b8627cbaeaa2ec091247e
Alyssa Bailey <alyssa.m.bailey@jpl.nasa.gov>
2026-03-12 08:42:49 -0700

    updated spaces in test

--- tests/npb/test__init.py
+++ tests/npb/test__init.py
@@ -3,11 +3,11 @@ import importlib
 
 
 def test_pds_namespace_import():
-     importlib.import_module('pds')
-     importlib.import_module('pds.naif_pds4_bundler')
+    importlib.import_module('pds')
+    importlib.import_module('pds.naif_pds4_bundler')
 
-     assert 'pds' in sys.modules
-     assert 'pds.naif_pds4_bundler' in sys.modules
+    assert 'pds' in sys.modules
+    assert 'pds.naif_pds4_bundler' in sys.modules
 
 
 def test_version_string_loaded():
