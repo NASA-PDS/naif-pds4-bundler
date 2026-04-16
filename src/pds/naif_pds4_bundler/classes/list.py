d89a22f0e4a0e86ce774883e5aff2639be00d230
aseco <mail@adrianseco.com>
2026-04-16 15:28:54 +0200

    Update check_products method in pds.naif_pds4_bundler.classes.list; replaced elif with if after continue statements.

--- src/pds/naif_pds4_bundler/classes/list.py
+++ src/pds/naif_pds4_bundler/classes/list.py
@@ -820,12 +820,14 @@ class KernelList:
                     "Product not present in any kernel directory(ies)"
                 )
                 continue
-            elif not origin_paths and ".tm" in product.lower():
+
+            if not origin_paths and ".tm" in product.lower():
                 product_warnings[product].append(
                     "Meta-kernel will be generated during this run."
                 )
                 continue
-            elif len(origin_paths) > 1:
+
+            if len(origin_paths) > 1:
                 product_warnings[product].append(
                     "Product present in multiple directories:"
                 )
